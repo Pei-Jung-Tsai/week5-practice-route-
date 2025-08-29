a268398210692bf107c97d702eeeb109160b26b5
peijung tsai <ptsa0008@student.monash.edu>
2025-08-29 13:11:41 +1000

    initial push

--- src/components/LibraryRegistrationForm.vue
+++ src/components/LibraryRegistrationForm.vue
@@ -6,6 +6,7 @@ import Column from 'primevue/column'
 const formData = ref({
   username: '',
   password: '',
+  confirmPassword: '',
   isAustralian: false,
   reason: '',
   gender: ''
@@ -35,10 +36,13 @@ const clearForm = () => {
 const errors = ref({
   username: null,
   password: null,
+  confirmPassword: null,
   resident: null,
   gender: null,
   reason: null
 })
+
+const reasonMessage = ref(null)
 
 const validateName = (blur) => {
   if (formData.value.username.length < 3) {
@@ -70,10 +74,24 @@ const validatePassword = (blur) => {
     errors.value.password = null
   }
 }
+
+const validateConfirmPassword = (blur) => {
+  if (formData.value.password !== formData.value.confirmPassword) {
+    if (blur) errors.value.confirmPassword = 'Passwords do not match.'
+  } else {
+    errors.value.confirmPassword = null
+  }
+}
+const validateReason = () => {
+  if (formData.value.reason.toLowerCase().includes('friend')) {
+    reasonMessage.value = 'Great to have a friend'
+  } else {
+    reasonMessage.value = null
+  }
+}
 </script>
 
 <template>
-  <!-- 🗄️ W3. Library Registration Form -->
   <div class="container mt-5">
     <div class="row">
       <div class="col-md-8 offset-md-2">
@@ -96,7 +114,17 @@ const validatePassword = (blur) => {
               />
               <div v-if="errors.username" class="text-danger">{{ errors.username }}</div>
             </div>
-
+              <div class="col-md-6 col-sm-6">
+              <label for="gender" class="form-label">Gender</label>
+              <select class="form-select" id="gender" v-model="formData.gender" required>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="other">Other</option>
+              </select>
+            </div>
+            </div>
+          
+            <div class="row mb-3">
             <div class="col-md-6 col-sm-6">
               <label for="password" class="form-label">Password</label>
               <input
@@ -109,7 +137,20 @@ const validatePassword = (blur) => {
               />
               <div v-if="errors.password" class="text-danger">{{ errors.password }}</div>
             </div>
-          </div>
+            <div class="col-md-6 col-sm-6">
+  <label for="confirm-password" class="form-label">Confirm password</label>
+  <input
+    type="password"
+    class="form-control"
+    id="confirm-password"
+    v-model="formData.confirmPassword"
+    @blur="() => validateConfirmPassword(true)"
+  />
+  <div v-if="errors.confirmPassword" class="text-danger">{{ errors.confirmPassword }}</div>
+</div>
+            </div>
+         
+          
           <div class="row mb-3">
             <div class="col-md-6 col-sm-6">
               <div class="form-check">
@@ -122,14 +163,7 @@ const validatePassword = (blur) => {
                 <label class="form-check-label" for="isAustralian">Australian Resident?</label>
               </div>
             </div>
-            <div class="col-md-6 col-sm-6">
-              <label for="gender" class="form-label">Gender</label>
-              <select class="form-select" id="gender" v-model="formData.gender" required>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-                <option value="other">Other</option>
-              </select>
-            </div>
+            
           </div>
           <div class="mb-3">
             <label for="reason" class="form-label">Reason for joining</label>
@@ -138,7 +172,11 @@ const validatePassword = (blur) => {
               id="reason"
               rows="3"
               v-model="formData.reason"
+              @input="validateReason"
             ></textarea>
+            <div v-if="reasonMessage" class="text-success">
+  {{ reasonMessage }}
+</div>
           </div>
           <div class="text-center">
             <button type="submit" class="btn btn-primary me-2">Submit</button>
